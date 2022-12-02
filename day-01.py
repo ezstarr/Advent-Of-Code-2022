ea67d2567b00ec91baad7358e0219fde1726e098
timeenjoyed <timeenjoyed.twitch@gmail.com>
2022-12-01 20:36:08 -0800

    one gold star

--- day-01.py
+++ day-01.py
@@ -1,12 +1,5 @@
 # How many calories are being carried by the elf carrying the most calories?
 
-# Progression:
-# For-loop
-# IndexError when indexing out of range
-# Now attempting to use functions to work around it <-- current
-# Next: Review how to stay in range using operations instead of enumeration etc.
-
-# WIP: NO ONE LOOK AT BELOW CODE XD
 
 with open('day-01-input.txt') as file:
     string_list = [line.strip() for line in file]
@@ -23,22 +16,15 @@ for string_line in string_list:
     int_line = int(string_line)
     converted_data.append(int_line)
 
-def get_a_group(food_cal_list):
 
-    one_elf = []
-    one_elf_sum = 0
-    for food in food_cal_list:
-        if food > 0:
-            one_elf.append(food)
-        else:
-            break
-    print(one_elf)
-    for item in one_elf:
-        one_elf_sum += item
-
-    return one_elf_sum
+total = 0
+for data in converted_data:
+    if data > 0:
+        total += data
+    if data == 0:
+        grouped_data.append(total)
+        total = 0
 
 
+print(max(grouped_data))
 
-
-print(get_a_group(converted_data))

4481f88e6b55e8da31c0c80f86be70e8b10d6caa
timeenjoyed <timeenjoyed.twitch@gmail.com>
2022-12-13 00:32:26 -0800

    wip part 2

--- day8-15/day-08.py
+++ day8-15/day-08.py
@@ -15,16 +15,15 @@ for tree_row in tree_grid:
 
     tree_array.append(row_list)
 
-
-
 print(*tree_array, sep="\n")
 
 
 def column(matrix, col):
     return [tree_row[col] for tree_row in matrix]
 
+
 print("================")
-print(column(tree_array, 1))
+# print(column(tree_array, 1))
 
 # for each tree[row][col],
 # check the left, top, right, bottom
@@ -39,7 +38,57 @@ hint: the "int" needs to to change (0 or len(tree_array)-1)
     
     for tree[2][2] -> 3 
 
-t_check = tree[0][2] <= tree[1][2] <= tree[2][2], then ok.
-r_check = tree[row][len(tree_row)-1+1] <= tree[row][len(tree_row)-1+2] <= until... tree[row][col] == tree[2][2]
-
+t_check = tree[0][2] <= tree[1][2] <= tree[2][2], then ok. 
+    // row + 1
+r_check = tree[2][(-1)-1] <= tree[2][(-1)-1] <= until... == tree[2][2]
+    // col - 1
+l_check = tree[2][0] <= tree[2][0+1] <= tree[2][0+2] <= tree[2][2]
+    // col + 1
+b_check = tree[(-1)][2] <= tree[(-1)-2][2] <= tree[2][2]
+    // row - 1
+    
 """
+
+
+def check_top(matrix, row, col):
+    i = 1
+    up_row = (row - i)
+    cur = tree_array[row][col]
+    up = tree_array[up_row][col]
+    print(f"cur: {cur}, up: {up}")
+
+    while up_row > 0:
+        if cur >= up:
+            print(f" cur >= up: {cur >= up}")
+            print(f" -first up_row: {up_row}")
+            i += 1
+            up_row = row - i
+            print(f" -next up_row: {up_row}")
+            print(f"  i equals {i}, row equals: {row}")
+            up = tree_array[up_row][col]
+            print(f"   cur: {cur}, up: {up}")
+            print(f"    cur >= up: {cur >= up}")
+        if up_row == 0:
+            visible = True
+            print(visible)
+        else:
+            print("not visible")
+
+
+
+
+def check_right(matrix, row):
+    pass
+
+
+def check_left(matrix, row):
+    pass
+
+
+def check_bottom(matrix, col):
+    pass
+
+
+# print(tree_array[(-1)-2][2] == tree_array[2][2])
+
+print(check_top(tree_array, 2, 0))

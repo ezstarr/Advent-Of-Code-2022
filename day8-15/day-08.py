d72a9c433ddd08259809fb3a04f1443ef3f161b2
timeenjoyed <timeenjoyed.twitch@gmail.com>
2022-12-15 03:38:58 -0800

    day 8 p2 wip

--- day8-15/day-08.py
+++ day8-15/day-08.py
@@ -1,5 +1,4 @@
-with open('day-08-input'
-          '.txt') as file:
+with open('day-08-example.txt') as file:
     tree_grid = [line.strip() for line in file.readlines()]
 
 tree_array = []
@@ -15,7 +14,6 @@ for tree_row in tree_grid:
 print(*tree_array, sep="\n")
 
 print("================")
-
 
 
 def check_top(matrix, row, col):
@@ -149,8 +147,6 @@ are_blocked = 0
 for r, tree_row in enumerate(tree_array):
     for c, tree in enumerate(tree_row):
         num_total_trees += 1
-        print(f"{r}, {c}")
-
         if (
                 check_top(tree_array, r, c) == 0 or
                 check_right(tree_array, r, c) == 0 or
@@ -159,12 +155,109 @@ for r, tree_row in enumerate(tree_array):
         ):
             pass
         else:
-            print(f" this tree is blocked {r}, {c}")
+            # print(f" this tree is blocked {r}, {c}")
             are_blocked += 1
 
-print(num_total_trees)
-print(are_blocked)
 visible_trees = num_total_trees - are_blocked
 print(visible_trees)
+
+# ==================== Part 2 ===========================
+
+def get_top_view(matrix, row, col):
+    up_row = row - 1
+    cur = tree_array[row][col]
+    up_tree = tree_array[up_row][col]
+    print(f"cur: {cur}, up_tree: {up_tree}")
+
+    if up_tree >= cur:
+        return 0
+
+    while up_tree < cur:
+        up_row -= 1
+        up_tree = tree_array[up_row][col]
+
+        if up_tree < cur:
+            up_row -= 1
+            up_tree = tree_array[up_row][col]
+
+        if up_tree <= cur:
+            return row - up_row
+
+
+# print(f"get top view: {get_top_view(tree_array, 4, 3)}")
+
+def get_right_view(matrix, row, col):
+    right_col = col + 1
+    cur = tree_array[row][col]
+    right_tree = tree_array[row][right_col]
+    print(f"cur: {cur}, right_tree: {right_tree}")
+
+    if right_tree >= cur:
+        return 0
+
+    while right_tree < cur:
+        right_col += 1
+        right_tree = tree_array[row][right_col]
+        right_col += 1
+
+        if right_tree >= cur:
+            return right_col - col
+
+        return right_col - col
+
+
+print(f"get right view: {get_right_view(tree_array, 2, 0)}")
+
+def get_left_view(matrix, row, col):
+    up_row = row - 1
+    cur = tree_array[row][col]
+    up_tree = tree_array[up_row][col]
+    print(f"cur: {cur}, up_tree: {up_tree}")
+
+    if up_tree >= cur:
+        return 0
+
+    while up_tree < cur:
+        up_row -= 1
+        up_tree = tree_array[up_row][col]
+
+        if up_tree < cur:
+            up_row -= 1
+            up_tree = tree_array[up_row][col]
+
+        if up_tree <= cur:
+            return row - up_row
+
+
+def get_bottom_view(matrix, row, col):
+    up_row = row - 1
+    cur = tree_array[row][col]
+    up_tree = tree_array[up_row][col]
+    print(f"cur: {cur}, up_tree: {up_tree}")
+
+    if up_tree >= cur:
+        return 0
+
+    while up_tree < cur:
+        up_row -= 1
+        up_tree = tree_array[up_row][col]
+
+        if up_tree < cur:
+            up_row -= 1
+            up_tree = tree_array[up_row][col]
+
+        if up_tree <= cur:
+            return row - up_row
+
+
+for r, tree_row in enumerate(tree_array):
+    for c, tree in enumerate(tree_row):
+        top_view = 0
+        right_view = 0
+        bottom_view = 0
+        left_view = 0
+
+
+        tree_scenic_score = top_view * right_view * bottom_view * left_view
 
 

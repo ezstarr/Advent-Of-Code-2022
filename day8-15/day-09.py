1becec1334c1dc9977d2eaa2e95852aa1ffc9fb9
timeenjoyed <timeenjoyed.twitch@gmail.com>
2022-12-20 01:04:24 -0800

    17th star fruit

--- day8-15/day-09.py
+++ day8-15/day-09.py
@@ -12,10 +12,8 @@ the tail always moves one step diagonally to keep up:
 You just need to work out where the tail goes as the head follows a series of motions.
 Assume the head and the tail both start at the same position, overlapping.
 
-
-
 """
-with open('day-09-example.txt') as file:
+with open('day-09-input.txt') as file:
     instructions = [line.strip() for line in file.readlines()]
 
 tails_been_here = []
@@ -24,11 +22,19 @@ h_position = (0, 0)
 
 t_position = (0, 0)
 
+tails_been_here.append(t_position)
+# grid = [['.' for x in range(6)] for y in range(5)]
+#
+# print(*grid, sep="\n")
+
 
 for line in instructions:
-    list_line = []
-    steps = int(line[2])
+    split_line = line.split()
+
+    steps = int(split_line[1])
     direction = line[0]
+
+    tails_been_here.append(t_position)
 
 
     if direction == 'R':
@@ -37,7 +43,7 @@ for line in instructions:
 
             # Tails follows heads horizontally:
             # Removing bc Y-axis doesn't actually matter:  if h_position[1] == t_position[1]:
-            if h_position[0] - t_position[0] == 1:
+            if h_position[0] - t_position[0] >= 1:
 
                 t_position = h_position
                 h_position = (h_position[0] + 1, h_position[1])
@@ -48,40 +54,36 @@ for line in instructions:
 
             else:
                 h_position = (h_position[0] + 1, h_position[1])
-
-
-            # if abs(h_position[1] - t_position[1]) <= 1:
-            #
-            #         h_position = (h_position[0] + 1, h_position[0])
-            #         print(f"  ==tails doesn't move==")
-            #         print(f"  new h_position: {h_position} \t new t_position: {t_position}")
-            #     if abs(h_position[1] - t_position[1]) > 1:
-            #         h_position = (h_position[0] + 1, h_position[0])
-            #         t_position = h_position
-            #         tails_been_here.append(t_position)
-            #         print(f"  ==tails moves diagonally==")
-            #         print(f"  new h_position: {h_position} \t new t_position: {t_position}")
-
+                tails_been_here.append(t_position)
+                print(f"  ==tails doesn't move==")
+                print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
     if direction == 'U':
-        if h_position[1] - t_position[1] == 1:
-
-            t_position = h_position
-            h_position = (h_position[0], h_position[1] + 1)
+        for i in range(1, steps + 1):
             tails_been_here.append(t_position)
+            print(f"i: {i}, \t dir: {direction}")
+            if h_position[1] - t_position[1] >= 1:
 
-            print(f"  ==tails follows heads==")
-            print(f"  new h_position: {h_position} \t new t_position: {t_position}")
+                t_position = h_position
+                h_position = (h_position[0], h_position[1] + 1)
+                tails_been_here.append(t_position)
 
-        else:
-            h_position = (h_position[0], h_position[1] + 1)
+                print(f"  ==tails follows heads==")
+                print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
+            else:
+                tails_been_here.append(t_position)
+                h_position = (h_position[0], h_position[1] + 1)
+                tails_been_here.append(t_position)
+                print(f"  ==tails doesn't move==")
+                print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
     if direction == 'L':
         for i in range(1, steps + 1):
+            tails_been_here.append(t_position)
             print(f"i: {i}, \t dir: {direction}")
 
-            if h_position[0] - t_position[0] == -1:
+            if h_position[0] - t_position[0] <= -1:
 
                 t_position = h_position
                 h_position = (h_position[0] - 1, h_position[1])
@@ -91,34 +93,42 @@ for line in instructions:
                 print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
             else:
+                tails_been_here.append(t_position)
                 h_position = (h_position[0] - 1, h_position[1])
+                tails_been_here.append(t_position)
+                print(f"  ==tails doesn't move==")
+                print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
     if direction == 'D':
         for i in range(1, steps + 1):
+            tails_been_here.append(t_position)
             print(f"i: {i}, \t dir: {direction}")
-            # Tails follows heads
-            if h_position[0] == t_position[0]:
+            if h_position[1] - t_position[1] <= -1:
+
+                t_position = h_position
                 h_position = (h_position[0], h_position[1] - 1)
-                t_position = (t_position[0], t_position[1] - 1)
                 tails_been_here.append(t_position)
+
                 print(f"  ==tails follows heads==")
                 print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
-            # Tail stays in place or moves to head's old position
-            if h_position[0] != t_position[0]:
+            else:
+                tails_been_here.append(t_position)
+                h_position = (h_position[0], h_position[1] - 1)
+                tails_been_here.append(t_position)
+                print(f"  ==tails doesn't move==")
+                print(f"  new h_position: {h_position} \t new t_position: {t_position}")
 
-                if abs(h_position[0] - t_position[0]) <= 1:
-                    h_position = (h_position[0], h_position[1] - 1)
-                    print(f"  ==tails doesn't move==")
-                    print(f"  new h_position: {h_position} \t new t_position: {t_position}")
-                if abs(h_position[1] - t_position[1]) > 1:
-                    h_position = (h_position[0], h_position[1] - 1)
-                    t_position = h_position
-                    tails_been_here.append(t_position)
-                    print(f"  ==tails moves diagonally==")
-                    print(f"  new h_position: {h_position} \t new t_position: {t_position}")
-
-
-
+# Part 2:
 print(len(set(tails_been_here)))
 
+#
+# def print_tail_positions(t_pos):
+#     grid = [['.' for x in range(6)] for y in range(5)]
+#     for x, y in t_pos:
+#         grid[y][x] = 'x'
+#     for row in reversed(grid):
+#         print(''.join(row))
+#
+#
+# print(print_tail_positions(tails_been_here))
